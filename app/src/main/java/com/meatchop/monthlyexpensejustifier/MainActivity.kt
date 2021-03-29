9c5d1cc1150594360d5732d462f5e07e10e67f01
arunabu <58628421+arunabu@users.noreply.github.com>
2021-03-29 17:50:42 +0530

    first commit

--- app/src/main/java/com/meatchop/monthlyexpensejustifier/MainActivity.kt
+++ app/src/main/java/com/meatchop/monthlyexpensejustifier/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.meatchop.monthlyexpensejustifier
 
+import android.annotation.SuppressLint
+import android.app.UiModeManager.MODE_NIGHT_NO
+import android.app.UiModeManager.MODE_NIGHT_YES
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
+
     }
 }
